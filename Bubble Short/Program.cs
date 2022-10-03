59c5ea0a62e0aabf4d18d7d8c6341fd482c971b3
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-10-03 21:45:04 +0700

    membuat fungsi dari algoritma bubble sort

--- Bubble Short/Program.cs
+++ Bubble Short/Program.cs
@@ -55,7 +55,24 @@ namespace Bubble_Short
             }
             Console.WriteLine("");
         }
-
+        public void BubblesortArray()
+        {
+            for (int i = 1; i < n; i++) //for n-1 passes
+            {
+                //pada pass i, bandingkan n-i elemen pertama dengan elemen selanjutnya
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (a[j] > a[j + 1])//jika elemen tidak dalam urutan yang benar
+                    {
+                        //tukar elemen 
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
             static void Main(string[] args)
         {
         }
